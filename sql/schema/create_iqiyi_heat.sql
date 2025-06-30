97efaf496116d655435f8c14a4cb583a9bd31421
XinGclover <cindygao7809@gmail.com>
2025-06-30 16:37:16 +0200

    modify iqiyi heat table,visualize the comparison of timeline

--- sql/schema/create_iqiyi_heat.sql
+++ sql/schema/create_iqiyi_heat.sql
@@ -14,3 +14,12 @@ SHOW timezone;
 
 
 SET TIMEZONE TO 'Asia/Shanghai';
+
+
+
+ALTER TABLE zhaoxuelu_heat_iqiyi ADD COLUMN timestamp_shanghai TIMESTAMP;
+
+--Convert to Shanghai time
+UPDATE zhaoxuelu_heat_iqiyi
+SET timestamp_shanghai = insert_time AT TIME ZONE 'Asia/Shanghai';
+ 
