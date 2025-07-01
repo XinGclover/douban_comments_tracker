0337c76de86f4eecbada348dcb0424c43569c282
XinGclover <cindygao7809@gmail.com>
2025-07-01 19:25:47 +0200

    get all collect pages of serveral user_ids

--- scraper/douban_user_ratings.py
+++ scraper/douban_user_ratings.py
@@ -10,7 +10,7 @@ from utils.common import safe_sleep
 from utils.config import BASE_URL, TABLE_PREFIX, DRAMA_TITLE
 from utils.config_loader import get_headers
 from utils.logger import setup_logger 
-from utils.html_tools import extract_href_info
+from utils.html_tools import extract_count
 
 
 setup_logger("logs/douban_user_ratings.log", logging.INFO)  
@@ -31,8 +31,7 @@ def parse_collect_block(block):
     rating_time = block.select_one('span.date').text.strip()
     comment_tag = block.select_one('span.comment')
     comment = comment_tag.text.strip() if comment_tag else None
-    vote_tag = block.select_one('span.pl')
-    vote_useful = vote_tag.text.strip() if vote_tag else None
+    vote_useful = extract_count(block, r'\((\d+)\s.*?\)', 'span.pl')
 
     return {
        "drama_id": drama_id,
@@ -63,31 +62,70 @@ def fetch_collect_page(page_num=0, headers=None, user_id=278582560):
 
         soup = BeautifulSoup(resp.text, "html.parser")
         total_page = fetch_total_page(soup)
+
         blocks = soup.find_all("div", class_="comment-item")
         parsed_blocks = [parse_collect_block(b) for b in blocks]
         if page_num == 0:
             return total_page, parsed_blocks
-        return parsed_blocks, None
+        return None, parsed_blocks
 
     except (requests.exceptions.RequestException, psycopg2.Error) as e:
         logging.error("Error: %s", e)
         return []
 
+def get_unfetched_user_ids(conn):
+    try:
+        cursor = conn.cursor()
+        limit = 2
+        query = """
+        SELECT user_id 
+        FROM low_rating_users
+        WHERE fetched = false
+        ORDER BY comment_time ASC
+        LIMIT %s
+        """
+        cursor.execute(query,(limit,))     
+        results = cursor.fetchall()
+        user_ids = [row[0] for row in results]
+
+        cursor.close()
+        conn.close()
+        return user_ids
+
+    except (psycopg2.Error, psycopg2.DatabaseError) as e:
+        logging.error(f"Database error: {e}")
+        return []
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
+        return []
+
+
+
 
 def main_loop():
-    request_headers = get_headers()  
-    total_page, first_page_drama =  fetch_collect_page(0, headers=request_headers, user_id=278582560)
- 
-    all_drama = first_page_drama
-    for page in range(1, total_page):
+    conn = get_db_conn()
+    request_headers = get_headers()
+    user_ids = get_unfetched_user_ids(conn) 
+    for user_id in user_ids:
         try:
-            _, drama = fetch_collect_page(page, headers=request_headers, user_id=278582560)
-            all_drama.extend(drama)
-           
-            safe_sleep(20, 30)
+            total_page, first_page_drama =  fetch_collect_page(0, headers=request_headers, user_id=user_id)
+            all_drama = first_page_drama.copy()
+
+            safe_sleep(20,30)
+            for page in range(1, total_page):
+                try:
+                    _, drama = fetch_collect_page(page, headers=request_headers, user_id=user_id)
+                    all_drama.extend(drama)
+                
+                    safe_sleep(20, 30)
+                except (requests.exceptions.RequestException, psycopg2.Error) as e:
+                    logging.error("Error in main_loop: %s", e)
+                    safe_sleep(10, 20)
+            safe_sleep(30, 50)
         except (requests.exceptions.RequestException, psycopg2.Error) as e:
-            logging.error("Error in main_loop: %s", e)
-            safe_sleep(10, 20)
+            logging.error("Error fetching first page: %s", e)
+
+
 
 
 if __name__ == "__main__":
